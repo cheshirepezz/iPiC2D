4effc402e12a15305c4d4e09de19ee8cf740637b
Luca Pezzini <cheshirepezz@protonmail.com>
2020-11-03 17:45:09 +0100

    Update

--- Fields/covTEz.py
+++ Fields/covTEz.py
@@ -73,8 +73,8 @@ def Plot3D(x, y, p):
 # STEP 1: SET the GRID!
 #
 
-Nt = 1500 # number of time steps
-Nx1, Nx2 = 500, 500  # nodes
+Nt = 100 # number of time steps
+Nx1, Nx2 = 75, 75  # nodes
 sigma = 0.02
 x1min, x1max = 0, 1 # physic domain x
 x2min, x2max = 0, 1 # physic domain y
@@ -106,16 +106,7 @@ Ex2 = np.zeros([Nx1, Nx2], dtype=float)
 Bx1 = np.zeros([Nx1, Nx2], dtype=float)
 Bx2 = np.zeros([Nx1, Nx2], dtype=float)
 Bx3 = np.zeros([Nx1, Nx2], dtype=float)
-Bx3old = np.zeros([Nx1, Nx2], dtype=float) # Bx3 at time n-1/2
-
-# Swop variable
-Ex1_l = np.zeros([Nx1, Nx2], dtype=float)
-Ex1_b = np.zeros([Nx1, Nx2], dtype=float)
-Ex2_l = np.zeros([Nx1, Nx2], dtype=float)
-Ex2_b = np.zeros([Nx1, Nx2], dtype=float)
-Bx3_l = np.zeros([Nx1, Nx2], dtype=float)
-Bx3_b = np.zeros([Nx1, Nx2], dtype=float)
-
+Bx3old = np.zeros([Nx1, Nx2], dtype=float)
 # Initialization of the Metric Tensor and the Jacobian
 J = np.ones([Nx1, Nx2], dtype=float) 
 gx1x1 = np.ones([Nx1, Nx2], dtype=float)
@@ -132,28 +123,57 @@ U = np.zeros([Nt], dtype=float) # Total energy
 divE = np.zeros([Nt], dtype=float) # Divergence of E
 Bx3[int((Nx1-1)/2),int((Nx2-1)/2)] = 0.001 # Initial condition
 
+def periodicBC(A):
+    A_w = np.zeros([Nx1, Nx2], dtype=float)
+    A_s = np.zeros([Nx1, Nx2], dtype=float)
+    A_b = np.zeros([Nx1, Nx2], dtype=float)
+    # swop var.
+    A_w[0, :] = A[0, :]
+    A_s[:, 0] = A[:, 0]
+    # Reflective BC for A field
+    A[0, :]  = A[-1, :]   # west = est
+    A[-1, :] = A_w[0, :]  # est = west
+    A[:, 0]  = A[:, -1]   # south = north
+    A[:, -1] = A_s[:, 0]  # north = south
+
+def fields(Ex1, Ex2, Bx3):
+    Bx3old[:, :] = Bx3[:, :]
+    Ex1[ib:ie, jb:je] += dt * (1./(dx2*J[ib:ie, jb:je]))\
+                                * (gx3x3[ib:ie, jb+1:je+1]*Bx3[ib:ie, jb+1:je+1] - gx3x3[ib:ie, jb:je]*Bx3[ib:ie, jb:je])
+    Ex2[ib:ie, jb:je] -= dt * (1./(dx1*J[ib:ie, jb:je]))\
+                                * (gx3x3[ib+1:ie+1, jb:je]*Bx3[ib+1:ie+1, jb:je] - gx3x3[ib:ie, jb:je]*Bx3[ib:ie, jb:je])
+    periodicBC(Ex1)
+    periodicBC(Ex2) 
+    Bx3[ib:ie, jb:je] -= dt * ((1./(2.*dx1*J[ib:ie, jb:je]))\
+                                *    (gx2x1[ib+1:ie+1, jb:je]*Ex1[ib+1:ie+1, jb:je] - gx2x1[ib-1:ie-1, jb:je]*Ex1[ib-1:ie-1, jb:je]\
+                                + 2.*(gx2x2[ib:ie, jb:je]*Ex2[ib:ie, jb:je] - gx2x2[ib-1:ie-1, jb:je]*Ex2[ib-1:ie-1, jb:je]))\
+                                - (1./(2.*dx2*J[ib:ie, jb:je]))\
+                                * (2.*(gx1x1[ib:ie, jb:je]*Ex1[ib:ie, jb:je] - gx1x1[ib:ie, jb-1:je-1]*Ex1[jb:je, ib-1:ie-1])\
+                                +      gx1x2[ib:ie, jb+1:je+1]*Ex2[ib:ie, jb+1:je+1] - gx1x2[ib:ie, jb-1:je-1]*Ex2[ib:ie, jb-1:je-1]))
+    periodicBC(Bx3)
+    return Ex1, Ex2, Bx3
+
 #
 # STEP 3: TIME EVOLUTION OF THE FIELD ON THE GRID!
 #
 
 # Start & End
 # Note we don't start from zero cause 0 and Nx1-1 are the same node
-x1s = 1
-x2s = 1
-x1e = Nx1 - 1
-x2e = Nx2 - 1
-
+ib = 1
+jb = 1
+ie = Nx1 - 1
+je = Nx2 - 1
 
 print("START SYSTEM EVOLUTION!")
 for t in range(Nt): # count {0, Nt-1}
     print("Time steps:", t)
     Bx3old[:, :] = Bx3[:, :]
-
+    '''
     # BEGIN : spatial update loops for Ey and Ex fields
-    Ex1[x1s:x1e, x2s:x2e] += dt * (1./(dx2*J[x1s:x1e, x2s:x2e]))\
-                                * (gx3x3[x1s:x1e, x2s+1:x2e+1]*Bx3[x1s:x1e, x2s+1:x2e+1] - gx3x3[x1s:x1e, x2s:x2e]*Bx3[x1s:x1e, x2s:x2e])
-    Ex2[x1s:x1e, x2s:x2e] -= dt * (1./(dx1*J[x1s:x1e, x2s:x2e]))\
-                                * (gx3x3[x1s+1:x1e+1, x2s:x2e]*Bx3[x1s+1:x1e+1, x2s:x2e] - gx3x3[x1s:x1e, x2s:x2e]*Bx3[x1s:x1e, x2s:x2e])
+    Ex1[ib:ie, jb:je] += dt * (1./(dx2*J[ib:ie, jb:je]))\
+                                * (gx3x3[ib:ie, jb+1:je+1]*Bx3[ib:ie, jb+1:je+1] - gx3x3[ib:ie, jb:je]*Bx3[ib:ie, jb:je])
+    Ex2[ib:ie, jb:je] -= dt * (1./(dx1*J[ib:ie, jb:je]))\
+                                * (gx3x3[ib+1:ie+1, jb:je]*Bx3[ib+1:ie+1, jb:je] - gx3x3[ib:ie, jb:je]*Bx3[ib:ie, jb:je])
     # END : spatial update loops for Ex1 and Ex2 fields
     
     # swop var.
@@ -172,12 +192,12 @@ for t in range(Nt): # count {0, Nt-1}
     Ex2[:, -1] = Ex2_b[:,0]  # top = bottom
     
     # BEGIN : spatial update loops for Bz fields
-    Bx3[x1s:x1e, x2s:x2e] -= dt * ((1./(2.*dx1*J[x1s:x1e, x2s:x2e]))\
-                                *    (gx2x1[x1s+1:x1e+1, x2s:x2e]*Ex1[x1s+1:x1e+1, x2s:x2e] - gx2x1[x1s-1:x1e-1, x2s:x2e]*Ex1[x1s-1:x1e-1, x2s:x2e]\
-                                + 2.*(gx2x2[x1s:x1e, x2s:x2e]*Ex2[x1s:x1e, x2s:x2e] - gx2x2[x1s-1:x1e-1, x2s:x2e]*Ex2[x1s-1:x1e-1, x2s:x2e]))\
-                                - (1./(2.*dx2*J[x1s:x1e, x2s:x2e]))\
-                                * (2.*(gx1x1[x1s:x1e, x2s:x2e]*Ex1[x1s:x1e, x2s:x2e] - gx1x1[x1s:x1e, x2s-1:x2e-1]*Ex1[x2s:x2e, x1s-1:x1e-1])\
-                                +      gx1x2[x1s:x1e, x2s+1:x2e+1]*Ex2[x1s:x1e, x2s+1:x2e+1] - gx1x2[x1s:x1e, x2s-1:x2e-1]*Ex2[x1s:x1e, x2s-1:x2e-1]))
+    Bx3[ib:ie, jb:je] -= dt * ((1./(2.*dx1*J[ib:ie, jb:je]))\
+                                *    (gx2x1[ib+1:ie+1, jb:je]*Ex1[ib+1:ie+1, jb:je] - gx2x1[ib-1:ie-1, jb:je]*Ex1[ib-1:ie-1, jb:je]\
+                                + 2.*(gx2x2[ib:ie, jb:je]*Ex2[ib:ie, jb:je] - gx2x2[ib-1:ie-1, jb:je]*Ex2[ib-1:ie-1, jb:je]))\
+                                - (1./(2.*dx2*J[ib:ie, jb:je]))\
+                                * (2.*(gx1x1[ib:ie, jb:je]*Ex1[ib:ie, jb:je] - gx1x1[ib:ie, jb-1:je-1]*Ex1[jb:je, ib-1:ie-1])\
+                                +      gx1x2[ib:ie, jb+1:je+1]*Ex2[ib:ie, jb+1:je+1] - gx1x2[ib:ie, jb-1:je-1]*Ex2[ib:ie, jb-1:je-1]))
     # END : spatial update loops for Bz fields
     
     # swop var.
@@ -188,13 +208,15 @@ for t in range(Nt): # count {0, Nt-1}
     Bx3[-1, :] = Bx3_l[0, :] # right = left
     Bx3[:, 0] = Bx3[:, -1]   # bottom = top
     Bx3[:, -1] = Bx3_b[:, 0] # top = bottom
+    '''
+    fields(Ex1, Ex2, Bx3)
 
     # Poynting theorem
-    U[t] =  0.5 * np.sum(np.power(Ex1[x1s:x1e, x2s:x2e],2.)\
-                      + np.power(Ex2[x1s:x1e, x2s:x2e],2.)\
-                      + Bx3[x1s:x1e, x2s:x2e] * Bx3old[x1s:x1e, x2s:x2e])
-    divE[t] = np.sum(1./J[x1s:x1e, x2s:x2e]*((1/dx1) * (J[x1s+1:x1e+1, x2s:x2e]*Ex1[x1s+1:x1e+1, x2s:x2e] - J[x1s:x1e, x2s:x2e]*Ex1[x1s:x1e, x2s:x2e])\
-                                           + (1/dx2) * (J[x1s:x1e, x2s+1:x2e+1]*Ex2[x1s:x1e, x2s+1:x2e+1] - J[x1s:x1e, x2s:x2e]*Ex2[x1s:x1e, x2s:x2e])))   
+    U[t] =  0.5 * np.sum(np.power(Ex1[ib:ie, jb:je],2.)\
+                      + np.power(Ex2[ib:ie, jb:je],2.)\
+                      + Bx3[ib:ie, jb:je] * Bx3old[ib:ie, jb:je])
+    divE[t] = np.sum(1./J[ib:ie, jb:je]*((1/dx1) * (J[ib+1:ie+1, jb:je]*Ex1[ib+1:ie+1, jb:je] - J[ib:ie, jb:je]*Ex1[ib:ie, jb:je])\
+                                           + (1/dx2) * (J[ib:ie, jb+1:je+1]*Ex2[ib:ie, jb+1:je+1] - J[ib:ie, jb:je]*Ex2[ib:ie, jb:je])))   
 print("DONE!")
 
 #
